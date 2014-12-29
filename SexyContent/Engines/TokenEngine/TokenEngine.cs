9398c87a6e529c6efa36f53ec1dad4197044c46f
Benjamin Gemperle <Benjamin.Gemperle@2sic.com>
2014-12-29 21:45:49 +0100

    Updated TokenEngine to use In-Streams instead of ModuleDataSource

--- SexyContent/Engines/TokenEngine/TokenEngine.cs
+++ SexyContent/Engines/TokenEngine/TokenEngine.cs
@@ -1,40 +1,48 @@
-﻿using System.Web;
+﻿using System.Globalization;
 using System.Web.Hosting;
-using DotNetNuke.Entities.Modules;
 using DotNetNuke.Entities.Portals;
-using DotNetNuke.UI.Modules;
 using System.Collections.Generic;
-using System.Collections;
 using System.Linq;
 using System.Text.RegularExpressions;
 using ToSic.Eav.DataSources;
-using ToSic.SexyContent.DataSources;
 
 namespace ToSic.SexyContent.Engines.TokenEngine
 {
-    public class TokenEngine : EngineBase
-    {
+	public class TokenEngine : EngineBase
+	{
+		private IEnumerable<DynamicEntity> GetEntitiesFromStream(IDictionary<string, IDataStream> streams, string streamName, string[] dimensions)
+		{
+			return streams.ContainsKey(streamName)
+				? streams[streamName].List.Select(e => new DynamicEntity(e.Value, dimensions, Sexy))
+				: new DynamicEntity[0];
+		}
 
-        /// <summary>
-        /// Renders a Token Template
-        /// </summary>
-        /// <returns>Rendered template as string</returns>
-        protected override string RenderTemplate()
-        {
-            DynamicEntity listContent = null;
-            DynamicEntity listPresentation = null;
+		/// <summary>
+		/// Renders a Token Template
+		/// </summary>
+		/// <returns>Rendered template as string</returns>
+		protected override string RenderTemplate()
+		{
+			// ToDo: Get rid of ModuleDataSource, use In-Streams instead
+			var dimensionIds = new[] { System.Threading.Thread.CurrentThread.CurrentCulture.Name };
+			var inStreams = ((IDataTarget)DataSource).In;
 
-            var moduleDataSource = DataPipelineFactory.FindDataSource<ModuleDataSource>((IDataTarget)DataSource);
-            listContent = moduleDataSource.ListElement != null ? moduleDataSource.ListElement.Content : null;
-            listPresentation = moduleDataSource.ListElement != null ? moduleDataSource.ListElement.Presentation : null;
-            var elements = moduleDataSource.ContentElements.Where(p => p.Content != null).ToList();
+			//var moduleDataSource = DataPipelineFactory.FindDataSource<ModuleDataSource>((IDataTarget)DataSource);
+			//listContent = moduleDataSource.ListElement != null ? moduleDataSource.ListElement.Content : null;
+			var listContent = GetEntitiesFromStream(inStreams, "ListContent", dimensionIds).FirstOrDefault();
+			//listPresentation = moduleDataSource.ListElement != null ? moduleDataSource.ListElement.Presentation : null;
+			var listPresentation = GetEntitiesFromStream(inStreams, "ListPresentation", dimensionIds).FirstOrDefault();
+			//var elements = moduleDataSource.ContentElements.Where(p => p.Content != null).ToList();
+			var elements = GetEntitiesFromStream(inStreams, "Default", dimensionIds);
+			var presentation = GetEntitiesFromStream(inStreams, "Presentation", dimensionIds).FirstOrDefault();
 
-            // Prepare Source Text
-            string sourceText = System.IO.File.ReadAllText(HostingEnvironment.MapPath(TemplatePath));
-            string repeatingPart;
-            
-            // Prepare List Object
-            var list = new Dictionary<string, string>
+
+			// Prepare Source Text
+			string sourceText = System.IO.File.ReadAllText(HostingEnvironment.MapPath(TemplatePath));
+			string repeatingPart;
+
+			// Prepare List Object
+			var list = new Dictionary<string, string>
             {
                 {"Index", "0"},
                 {"Index1", "1"},
@@ -47,48 +55,55 @@ namespace ToSic.SexyContent.Engines.TokenEngine
                 {"Alternator5", "0"}
             };
 
-            list["Count"] = elements.Count.ToString();
+			var elementsCount = elements.Count();
+			list["Count"] = elementsCount.ToString(CultureInfo.InvariantCulture);
 
-            // If the SourceText contains a <repeat>, define Repeating Part. Else take SourceText as repeating part.
-            bool containsRepeat = sourceText.Contains("<repeat>") && sourceText.Contains("</repeat>");
-            if (containsRepeat)
-                repeatingPart = Regex.Match(sourceText, @"<repeat>(.*?)</repeat>", RegexOptions.Multiline | RegexOptions.IgnoreCase | RegexOptions.Singleline).Groups[1].Captures[0].Value;
-            else
-                repeatingPart = "";
+			// If the SourceText contains a <repeat>, define Repeating Part. Else take SourceText as repeating part.
+			var containsRepeat = sourceText.Contains("<repeat>") && sourceText.Contains("</repeat>");
+			if (containsRepeat)
+				repeatingPart = Regex.Match(sourceText, @"<repeat>(.*?)</repeat>", RegexOptions.Multiline | RegexOptions.IgnoreCase | RegexOptions.Singleline).Groups[1].Captures[0].Value;
+			else
+				repeatingPart = "";
 
-            string renderedTemplate = "";
+			var renderedTemplate = "";
 
-            foreach (Element element in elements)
-            {
-                // Modify List Object
-                list["Index"] = elements.IndexOf(element).ToString();
-                list["Index1"] = (elements.IndexOf(element) + 1).ToString();
-                list["IsFirst"] = elements.First() == element ? "First" : "";
-                list["IsLast"] = elements.Last() == element ? "Last" : "";
-                list["Alternator2"] = (elements.IndexOf(element) % 2).ToString();
-                list["Alternator3"] = (elements.IndexOf(element) % 3).ToString();
-                list["Alternator4"] = (elements.IndexOf(element) % 4).ToString();
-                list["Alternator5"] = (elements.IndexOf(element) % 5).ToString();
+			var elementIndex = 0;
+			foreach (var element in elements)
+			{
+				// Modify List Object
+				list["Index"] = elementIndex.ToString();
+				list["Index1"] = (elementIndex + 1).ToString();
+				list["IsFirst"] = elementIndex == 0 ? "First" : "";
+				list["IsLast"] = elementsCount == elementsCount - 1 ? "Last" : "";
+				list["Alternator2"] = (elementIndex % 2).ToString();
+				list["Alternator3"] = (elementIndex % 3).ToString();
+				list["Alternator4"] = (elementIndex % 4).ToString();
+				list["Alternator5"] = (elementIndex % 5).ToString();
 
-                // Replace Tokens
-                var tokenReplace = new TokenReplace(element.Content, element.Presentation, listContent, listPresentation, list, App);
-                tokenReplace.ModuleId = ModuleInfo.ModuleID;
-                tokenReplace.PortalSettings = PortalSettings.Current;
-                renderedTemplate += tokenReplace.ReplaceEnvironmentTokens(repeatingPart);
-            }
+				// Replace Tokens
+				DynamicEntity listItemPresentation = null; // ToDo: get correct Presentation-Entity
+				var tokenReplace = new TokenReplace(element, listItemPresentation, listContent, listPresentation, list, App)
+				{
+					ModuleId = ModuleInfo.ModuleID,
+					PortalSettings = PortalSettings.Current
+				};
+				renderedTemplate += tokenReplace.ReplaceEnvironmentTokens(repeatingPart);
 
-            // Replace repeating part
-            renderedTemplate = Regex.Replace(sourceText, "<repeat>.*?</repeat>", renderedTemplate, RegexOptions.Multiline | RegexOptions.IgnoreCase | RegexOptions.Singleline);
+				elementIndex++;
+			}
 
-            // Replace tokens outside the repeating part
-            var tr2 = new TokenReplace(elements.Any() ? elements.First().Content : null, elements.Any() ? elements.First().Presentation : null, listContent, listPresentation, list, App);
-            tr2.ModuleId = ModuleInfo.ModuleID;
-            tr2.PortalSettings = PortalSettings.Current;
-            renderedTemplate = tr2.ReplaceEnvironmentTokens(renderedTemplate);
+			// Replace repeating part
+			renderedTemplate = Regex.Replace(sourceText, "<repeat>.*?</repeat>", renderedTemplate, RegexOptions.Multiline | RegexOptions.IgnoreCase | RegexOptions.Singleline);
 
-            return renderedTemplate;
-        }
+			// Replace tokens outside the repeating part
+			var tr2 = new TokenReplace(elements.Any() ? elements.First() : null, elements.Any() ? presentation : null, listContent, listPresentation, list, App)
+			{
+				ModuleId = ModuleInfo.ModuleID,
+				PortalSettings = PortalSettings.Current
+			};
+			renderedTemplate = tr2.ReplaceEnvironmentTokens(renderedTemplate);
 
-
-    }
+			return renderedTemplate;
+		}
+	}
 }
